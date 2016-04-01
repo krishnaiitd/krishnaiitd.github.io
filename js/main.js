c61d2311fdf05e8bf3d069f53db7caed7efe5b57
krishnaiitd <kprasad.iitd@gmail.com>
2016-04-02 00:24:18 +0530

    Added pop up window for under contruction info

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log('This would be the main JS file.');
+// Trigger the modal with a button
+$(function() {
+
+	$("#develop-info-dismiss").on('click', function() {
+		setCookie('closed', '1', 1);
+	});
+
+	$("#develop-info-close").on('click', function() {
+		setCookie('closed', '1', 1);
+	});
+
+	var cookie = getCookie('closed');
+	console.log(cookie);
+	if (cookie === "" || cookie === null || cookie === undefined) {
+		setInterval(function(){
+			$("#develop-info").modal('show');
+		}, 8000);
+	}
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+		}
+		return "";
+	}
+
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+});
